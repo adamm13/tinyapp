3dec7743f1c9aef1cdbd50d7190a8ddb8cab387e
Adam Mohammed <13.adamm@gmail.com>
2021-02-16 15:07:49 -0500

    updated to add Delete button to remove URLs

--- express_server.js
+++ express_server.js
@@ -1,29 +1,33 @@
+//require express
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//use body parser middleware before handlers; use req.body to access
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//set view engine to ejs
 app.set('view engine', 'ejs');
 
+//funciton to generate random string
 const generateRandomString = function(){
   let length = 6
   return Math.random().toString(20).substr(2, length)
 }
-
+//practice 'database'
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
   "4fn9sf": "http://www.reddit.com"
 };
 
-
+// URL page 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//create a new URL page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -34,15 +38,24 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// Delete URL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  //console.log (req.params.shortURL)
+  delete urlDatabase[req.params.shortURL];
+  res.redirect(`/urls`);
+});
+
+//takes the new input and generates the new string code - redirects to home page
 app.post("/urls", (req,res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   //console.log(req.body);   returns { longURL: 'www.website.com' }
-  res.redirect(`/urls/${shortURL}`);
+  res.redirect(`/urls`); //add /${shortURL} to redirect to short URL page with new INFO. 
 });
 
+//shows your individual link and its short/ long
 app.get("/u/:shortURL", (req, res) => { //redirect to longURL page
-   const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
 
