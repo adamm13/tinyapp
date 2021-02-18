b4e43fa94dbc4fc2b25ee391ee3b2cc3a4431ccb
Adam Mohammed <13.adamm@gmail.com>
2021-02-18 14:03:13 -0500

    added function to find urls by user ID

--- helpers.js
+++ helpers.js
@@ -1,12 +1,31 @@
 
 
-const findUserEmail = (email, users) => {
+const findUserByEmail = (email, users) => {
   return Object.values(users).find(user => user.email === email);
 };
+//TESTESTTESTTEST: {
+ // b6UTxQ: { longURL: 'https://www.tsn.ca', userID: 'aJ48lW' },
+ // i3BoGr: { longURL: 'https://www.google.ca', userID: 'aJ48lW' },
+  //'0g4ihf': { longURL: 'http://www.gamestop.com', userID: 'adamm13' }
+//}
+const findUrlsByUserID = (urls, id) => {
+  let usersUrls = {};
+  for (let url in urls) {
+    //console.log('pre condition:', urls[url]);
+    //console.log('pre condition:', urls[url].userID);
+    if (urls[url].userID === id) {
+      //console.log('POST condition', id);
+      //console.log('POST condition', urls[url]);
+      //console.log('POST condition', urls[url].userID);
+      usersUrls[url] = urls[url];
+    }
+  }
+  return usersUrls;
+}
 
 const findUser = (email, users) => {
-  const currentUser = users.find(userObj => userObj.email === email)
+  const currentUser = users.find(userObj => userObj.email === email);
 
-  return currentUser
+  return currentUser;
 }
-module.exports = { findUserEmail, findUser }
+module.exports = { findUserByEmail, findUser, findUrlsByUserID  }
